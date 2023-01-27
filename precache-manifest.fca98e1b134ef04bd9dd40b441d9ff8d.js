943e05c9344e71281cb1579fb3649ec482e915da
it21060380 <it21060380@my.sliit.lk>
2023-01-27 11:19:41 +0530

    Updates

---
+++ precache-manifest.fca98e1b134ef04bd9dd40b441d9ff8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c60605208e7bc1354a1c",
+    "url": "/website/static/css/main.835045d3.chunk.css"
+  },
+  {
+    "revision": "c60605208e7bc1354a1c",
+    "url": "/website/static/js/main.c6060520.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/website/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/website/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "98bb58bc946acbcccf0b",
+    "url": "/website/static/js/runtime~main.98bb58bc.js"
+  },
+  {
+    "revision": "220b20fec5d926aed29f33e1c651adb7",
+    "url": "/website/index.html"
+  }
+];
